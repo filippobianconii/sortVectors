f835f879001cc11fe860a22620fc08b4cf067ff9
filippo.bianconi <filippo.bianconi.stud@ispascalcomandini.it>
2020-10-07 11:06:31 +0200

    First step

---
+++ sortVectors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] vett = new int [10]  {4,-14,100,65,76,89,54,57,78,90};
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(vett[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
