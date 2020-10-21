52c90d9c3549f142dd2f745beaf60110b171ef80
squirlyfoxy <public.thesquirlyz97@gmail.com>
2020-10-21 11:20:50 +0200

    Aggiunto metedo per il calcolo del massimo

--- sortVectors/Program.cs
+++ sortVectors/Program.cs
@@ -7,10 +7,32 @@ namespace sortVectors
         static void Main(string[] args)
         {
             int [] vett = new int [10]  {4,-14,100,65,76,89,54,57,78,90};
-            for (int i = 0; i < 10; i++)
-                Console.WriteLine(vett[i]);
+            for (int i = 0; i < vett.Length; i++)
+                Console.Write(vett[i] + ", ");
+
+            Ordina(vett);
+
+            Console.WriteLine();
+            for (int i = 0; i < vett.Length; i++)
+                Console.Write(vett[i] + ", ");
+
+            Console.WriteLine();
+            Console.WriteLine("Il valore massimo è: " + Massimo(vett));
 
             Console.ReadKey();
+        }
+
+        static void Ordina(int[] v)
+        {
+            Array.Sort(v);
+        }
+
+        static int Massimo(int[] v)
+        {
+            if (v.Length == 0)
+                return v[0];
+
+            return v[v.Length - 1];
         }
     }
 }
